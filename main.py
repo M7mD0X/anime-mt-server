912aa9369f5e98b70538668b3f6b873cdd73f3c1
Muhammed <85457690+M7mD0X@users.noreply.github.com>
2026-02-25 21:27:47 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import anime, episodes, admin
+from routes import anime, episodes, admin, scraper
 from database import connect_db
 
 app = FastAPI(title="Anime MT API")
@@ -23,3 +23,4 @@ async def root():
 app.include_router(anime.router, prefix="/anime", tags=["Anime"])
 app.include_router(episodes.router, prefix="/episodes", tags=["Episodes"])
 app.include_router(admin.router, prefix="/admin", tags=["Admin"])
+app.include_router(scraper.router, prefix="/scraper", tags=["Scraper"])
